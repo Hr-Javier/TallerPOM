89e628f6e46fa9a84543a39fa6a9fefefdfa56d7
Hr-Javier <48157492+Hr-Javier@users.noreply.github.com>
2023-01-27 13:59:55 -0500

    Proyecto tallerPOM

--- src/test/java/definition/DefinitionTaller.java
+++ src/test/java/definition/DefinitionTaller.java
@@ -14,28 +14,23 @@ public class DefinitionTaller {
     }
 
     @Given("^estoy en la pagina \"([^\"]*)\"$")
-    public void estoyEnLaPagina(String arg1) throws Exception {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void estoyEnLaPagina(String arg1) {
+
     }
 
-    @When("^Ingrese las credenciales$")
-    public void ingreseLasCredenciales(DataTable arg1) throws Exception {
-        // Write code here that turns the phrase above into concrete actions
-        // For automatic transformation, change DataTable to one of
-        // List<YourType>, List<List<E>>, List<Map<K,V>> or Map<K,V>.
-        // E,K,V must be a scalar (String, Integer, Date, enum etc)
-        throw new PendingException();
+
+        @When("^Ingrese las credenciales$")
+        public void ingreseLasCredenciales (){
+
+        }
+
+        @Then("^valido que el usuario creado tenga asignado un numero$")
+        public void validoQueElUsuarioCreadoTengaAsignadoUnNumero () {
+        }
+
+        @After
+        public void afterTest() {
+            System.out.println("@After");
+
+        }
     }
-
-    @Then("^valido que el usuario creado tenga asignado un numero$")
-    public void validoQueElUsuarioCreadoTengaAsignadoUnNumero() throws Exception {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
-    }
-
-    @After
-    public void afterTest(){
-        System.out.println("@After");
-
-}
